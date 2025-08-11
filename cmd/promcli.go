2a7047b18c2a9081bb36a95da4b148778f00ba87
Wil Cram <wmcram@amazon.com>
2025-08-11 19:06:30 +0000

    support filtering by name and label

--- cmd/promcli.go
+++ cmd/promcli.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
-	"os"
+	"strings"
 
 	"github.com/prometheus/common/expfmt"
 	display "github.com/wmcram/prom-cli"
@@ -12,19 +13,53 @@ import (
 
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: promcli <endpoint>")
+	var nameFilter *string = flag.String("name", "", "Filter metrics by name: NAME1,NAME2,...")
+	var labelFilter *string = flag.String("label", "", "Filter metrics by label: LABEL1=VAL1,LABEL2=VAL2,...")
+
+	flag.Parse()
+
+	endpoints := flag.Args()
+	if len(endpoints) == 0 {
+		fmt.Println("Usage: promcli <endpoints>")
 		return
 	}
-	endpoint := os.Args[1]
 
-	resp, err := http.Get(endpoint)
-	if err != nil {
-		fmt.Println("Error reaching endpoint:", err)
-		return
+	for _, endpoint := range endpoints {
+		resp, err := http.Get(endpoint)
+		if err != nil {
+			fmt.Println("Error reaching endpoint:", err)
+			return
+		}
+		defer resp.Body.Close()
+
+		decoder := expfmt.NewDecoder(resp.Body, expfmt.NewFormat(expfmt.TypeTextPlain))
+		display.DisplayMetrics(decoder, endpoint, processNameFilter(*nameFilter), processLabelFilter(*labelFilter))
 	}
-	defer resp.Body.Close()
-
-	decoder := expfmt.NewDecoder(resp.Body, expfmt.NewFormat(expfmt.TypeTextPlain))
-	display.DisplayMetrics(decoder)
 }
+
+func processNameFilter(filter string) map[string]bool {
+	if filter == "" {
+		return nil
+	}
+	m := make(map[string]bool)
+	for _, name := range strings.Split(filter, ",") {
+		m[name] = true
+	}
+	return m
+}
+
+func processLabelFilter(filter string) map[string]string {
+	if filter == "" {
+		return nil
+	}
+	m := make(map[string]string)
+	for pair := range strings.SplitSeq(filter, ", ") {
+		parts := strings.Split(pair, "=")
+		if len(parts) != 2 {
+			continue
+		}
+		m[parts[0]] = parts[1]
+	}
+	return m
+}
+
--- display.go
+++ display.go
@@ -2,26 +2,55 @@ package display
 
 import (
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/fatih/color"
 	dto "github.com/prometheus/client_model/go"
 	"github.com/prometheus/common/expfmt"
+	"golang.org/x/term"
 )
 
-const TermWidth = 80
+var TermWidth, TermHeight int
 
-func DisplayMetrics(decoder expfmt.Decoder) {
+// init gets the terminal dimensions.
+func init() {
+	var err error
+	TermWidth, TermHeight, err = term.GetSize(int(os.Stdin.Fd()))
+	if err != nil {
+		TermWidth = 80
+		TermHeight = 24
+	}
+}
+
+var (
+	endpointColor = color.New(color.BgMagenta)
+	titleColor    = color.New(color.BgCyan, color.FgWhite)
+	valueColor    = color.New(color.FgBlack, color.BgGreen, color.Bold)
+	labelColor    = color.New(color.FgBlack, color.BgYellow)
+)
+
+// DisplayMetrics prints the metrics to the screen after filtering.
+func DisplayMetrics(decoder expfmt.Decoder, endpoint string, nameFilter map[string]bool, labelFilter map[string]string) {
+	endpointColor.Printf("Endpoint %s\n", endpoint)
 	for {
 		mf := dto.MetricFamily{}
 		err := decoder.Decode(&mf)
 		if err != nil {
 			break
 		}
+		if nameFilter != nil && !nameFilter[mf.GetName()] {
+			continue
+		}
+
 		metricType := mf.GetType()
-		color.Cyan("%s (%s): %s", mf.GetName(), metricType, mf.GetHelp())
+		titleColor.Printf("%s (%s): %s\n", mf.GetName(), metricType, mf.GetHelp())
+
 		for _, metric := range mf.Metric {
-			color.Cyan(strings.Repeat("-", TermWidth))
+			if labelFilter != nil && !matchesLabelFilter(metric, labelFilter) {
+				continue
+			}
+
 			var labelStrings = []string{}
 			for _, labelPair := range metric.GetLabel() {
 				labelStrings = append(labelStrings, fmt.Sprintf("%s=\"%s\"", labelPair.GetName(), labelPair.GetValue()))
@@ -31,12 +60,22 @@ func DisplayMetrics(decoder expfmt.Decoder) {
 			switch metricType {
 			// only support counter and gauge for now
 			case dto.MetricType_COUNTER:
-				color.Green("> %.0f", metric.GetCounter().GetValue())
+				valueColor.Printf("%.0f\n", metric.GetCounter().GetValue())
 			case dto.MetricType_GAUGE:
-				color.Green("> %.2f", metric.GetGauge().GetValue())
+				valueColor.Printf("%.2f\n", metric.GetGauge().GetValue())
 			}
-			color.Yellow(labels)
+			labelColor.Printf("%s\n", labels)
 		}
-		color.Cyan(strings.Repeat("-", TermWidth))
 	}
+}
+
+// matchesLabelFilter determines whether this metric is matched by the given labelFilter
+func matchesLabelFilter(metric *dto.Metric, labelFilter map[string]string) bool {
+	goal := len(labelFilter)
+	for _, labelPair := range metric.GetLabel() {
+		if labelFilter[labelPair.GetName()] == labelPair.GetValue() {
+			goal--
+		}
+	}
+	return goal == 0
 }
